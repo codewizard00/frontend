e6b44bc79a5b2ce4472de3c761d7425bdc4a80b5
Ganesh Negi <ganesh.negi@finbii.com>
2023-02-28 22:26:52 +0530

    new changes

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -47,7 +47,7 @@ const Team = () => {
                                     </div>
                                     <div class="px-6 mt-16">
                                         <h1 class="font-bold dark:text-white text-3xl text-center mb-1">{value.fullName}</h1>
-                                        <p class="text-gray-800 dark:text-white text-sm text-center">{value.place}</p>
+                                        <p class="text-gray-800 dark:text-white text-sm text-center">{value.place} ,{value.position}</p>
                                         <p class="text-center text-gray-600 dark:text-gray-200 text-base pt-3 font-normal">{value.about}</p>
                                         {/* <div class="w-full flex justify-center pt-5 pb-5">
                                             <a href="javascript:void(0)" class="mx-5">
