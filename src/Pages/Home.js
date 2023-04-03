1b04b58be5fbf2a9cd02e9b2d7966d1258b28914
Ganesh Negi <ganesh.negi@finbii.com>
2023-04-04 00:27:00 +0530

    new changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,6 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import BookCarousel from "../BooksShow/BookCarousel"
 import BookShow from "../BooksShow/BookeShow"
-import ImageCarousel from "../Carousel/Carousel"
 import EventGallery from "../EventGallery/EventGallery"
 import Footer from "../Footer/Footer"
 import HeroSection from "../HeroSections/HeroSection"
@@ -14,6 +13,7 @@ import Writer from "../Writer/Writer"
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const [loader, setLoader] = useState(true);
     const getAllBanner = () => {
         var config = {
             method: 'get',
@@ -31,57 +31,68 @@ const Home = () => {
     }
 
     useEffect(() => {
+        window.addEventListener('load', (event) => {
+            setLoader(false)
+        });
         getAllBanner();
     }, [])
 
+
+
+
     return (
         <>
-            <NavBar />
-            <HomeCarousel data={data}/>
-            <BookCarousel />
-            <BookShow />
-            <EventGallery />
-            <Writer />
-            <CarouselCards name={"Blog"} />
-            <HeroSection left={true} />
-            <CarouselCards name={"Competitions"} />
-            <CarouselCards name={"Upcoming-Competitions"} />
-            <section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
-                <div class="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
-                    <div class="max-w-2xl mx-auto text-center">
-                        <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Numbers tell our story</h2>
-                        <p class="mt-3 text-xl leading-relaxed text-gray-600 md:mt-8">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
+            {loader ?
+                <img className="h-[100vh] w-[100vw]" src="https://res.cloudinary.com/valarmorghullis/image/upload/v1680548038/loader_n5agsp.jpg"/> :
+                <>
+        <NavBar />
+        <HomeCarousel data={data}/>
+        <BookCarousel />
+        <BookShow />
+        <EventGallery />
+        <Writer />
+        <CarouselCards name={"Blog"} />
+        <HeroSection left={true} />
+        <CarouselCards name={"Competitions"} />
+        <CarouselCards name={"Upcoming-Competitions"} />
+        <section class="py-10 bg-gray-100 sm:py-16 lg:py-24">
+            <div class="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
+                <div class="max-w-2xl mx-auto text-center">
+                    <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">Numbers tell our story</h2>
+                    <p class="mt-3 text-xl leading-relaxed text-gray-600 md:mt-8">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis.</p>
+                </div>
+
+                <div class="grid grid-cols-1 gap-8 mt-10 text-center lg:mt-24 sm:gap-x-8 md:grid-cols-3">
+                    <div>
+                        <h3 class="font-bold text-7xl">
+                            <span class="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-600"> 6+ </span>
+                        </h3>
+                        <p class="mt-4 text-xl font-medium text-gray-900">Years in business</p>
+                        <p class="text-base mt-0.5 text-gray-500">Creating the successful path</p>
                     </div>
 
-                    <div class="grid grid-cols-1 gap-8 mt-10 text-center lg:mt-24 sm:gap-x-8 md:grid-cols-3">
-                        <div>
-                            <h3 class="font-bold text-7xl">
-                                <span class="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-600"> 6+ </span>
-                            </h3>
-                            <p class="mt-4 text-xl font-medium text-gray-900">Years in business</p>
-                            <p class="text-base mt-0.5 text-gray-500">Creating the successful path</p>
-                        </div>
+                    <div>
+                        <h3 class="font-bold text-7xl">
+                            <span class="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-600"> 4821 </span>
+                        </h3>
+                        <p class="mt-4 text-xl font-medium text-gray-900">Projects delivered</p>
+                        <p class="text-base mt-0.5 text-gray-500">In last 6 years</p>
+                    </div>
 
-                        <div>
-                            <h3 class="font-bold text-7xl">
-                                <span class="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-600"> 4821 </span>
-                            </h3>
-                            <p class="mt-4 text-xl font-medium text-gray-900">Projects delivered</p>
-                            <p class="text-base mt-0.5 text-gray-500">In last 6 years</p>
-                        </div>
-
-                        <div>
-                            <h3 class="font-bold text-7xl">
-                                <span class="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-600"> 37+ </span>
-                            </h3>
-                            <p class="mt-4 text-xl font-medium text-gray-900">Team members</p>
-                            <p class="text-base mt-0.5 text-gray-500">Working for your success</p>
-                        </div>
+                    <div>
+                        <h3 class="font-bold text-7xl">
+                            <span class="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-600 to-blue-600"> 37+ </span>
+                        </h3>
+                        <p class="mt-4 text-xl font-medium text-gray-900">Team members</p>
+                        <p class="text-base mt-0.5 text-gray-500">Working for your success</p>
                     </div>
                 </div>
-            </section>
-            <Newsletter />
-            <Footer />
+            </div>
+        </section>
+        <Newsletter />
+        <Footer />
+        </>
+            }
 
         </>
     )
