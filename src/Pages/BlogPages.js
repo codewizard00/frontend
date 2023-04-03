d11e9086439c925219a6cbb27935f28a937836c2
Ganesh Negi <ganesh.negi@finbii.com>
2023-04-04 01:33:34 +0530

    new changes

--- src/Pages/BlogPages.js
+++ src/Pages/BlogPages.js
@@ -16,7 +16,6 @@ const BlogPages = () => {
             url: `${process.env.REACT_APP_PROD_URL}get/competition/${id}`,
             headers: {}
         };
-
         axios(config)
             .then(function (response) {
                 setData(response.data.message)
@@ -38,19 +37,23 @@ const BlogPages = () => {
                 <Link to={`/competition/${data.id}`}>  परिणाम घोषित हो गए हैं चेक करने के लिए यहां क्लिक करें</Link>
             </div>
             <main class="mt-10">
-                <div class="mb-4 md:mb-0 w-full max-w-screen-xl mx-auto relative" style={{ height: "24em" }}>
+               <div class="mb-4 md:mb-0 w-full max-w-screen-xl mx-auto " style={{ height: "24em" }}>
                     <div class="absolute left-0 bottom-0 w-full h-full z-10"
                         style={{ backgroundImage: "linear-gradient(180deg  transparent,rgba(0,0,0,.7))" }}></div>
-                    <img src={`${data.image_url}`} class="absolute left-0 top-0 w-full h-full z-0 object-cover" />
-                    <div class="p-4 absolute bottom-0 left-0 z-20">
+                        <div className="flex justify-center">
+                    <img src={`${data.image_url}`} class=" hidden md:flex margin-auto justify-center  object-cover aspect-auto " />
+                    <img src={`${data.image_mobile_url}`} class=" block md:hidden object-cover aspect-video" />
+                    </div>
+                  
+                    <div class="p-4 z-50">
 
-                        <h2 class="text-4xl font-semibold text-gray-100 leading-tight">
+                        <h2 class="text-4xl font-semibold text-gray-700 leading-tight">
                             {data.title}
                         </h2>
                         <div class="mt-3">
                             <div>
-                                <p class="font-semibold text-gray-200 text-sm">{data.about} </p>
-                                <p class="font-semibold text-gray-400 text-xs"> {data.timings} </p>
+                                <p class="font-semibold text-gray-700 text-sm">{data.about} </p>
+                                <p class="font-semibold text-gray-700 text-xs"> {data.timings} </p>
                             </div>
                             <div className="gap-1 my-2 flex">
                                 {data?.keyword?.split(",")?.map((data1, index) => (
@@ -62,7 +65,7 @@ const BlogPages = () => {
                     </div>
                 </div>
 
-                <div class="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-lg mx-auto text-lg leading-relaxed">
+                <div class="px-4 lg:px-0 mt-16 sm:mt-40 text-gray-700 max-w-screen-lg mx-auto text-lg leading-relaxed">
                     <div dangerouslySetInnerHTML={{ __html: data.content }} />
 
                 </div>
